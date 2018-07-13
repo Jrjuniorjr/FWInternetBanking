44668c859644550e0a9d65e9156e0664d4bdc6d5
LLealL <llealdegoes@gmail.com>
2018-07-13 09:49:55 -0300

    implementacao metodo renderJurosTotal para teste

--- src/fwIB/Teste.java
+++ src/fwIB/Teste.java
@@ -3,17 +3,40 @@ package fwIB;
 public class Teste {
 
 	public static void main(String[] args) {
-		Conta a,b;
+
 		RepositorioContasArray contas = new RepositorioContasArray();
-		a=new Conta("1213-7");
-		b=new Conta("44418");
-		contas.inserirConta(a);
-		contas.inserirConta(b);
-		a.transferir(b, 20.0);
-		Conta conta = contas.procurarConta("1213-7");
-		System.out.println("Numero da conta " + conta.getNumero());
-		System.out.println("Saldo da conta " + conta.consultarSaldo());
+
+		contas.inserirConta(new Conta ("1227-7",100));
+
+		contas.inserirConta(new Conta ("1228-7",100));
+
+		contas.inserirConta(new Conta ("1225-7",100));
+
+		contas.inserirConta(new Poupanca ("1224-7",100));
+
+		contas.inserirConta(new Poupanca ("1223-7",100));
+
+		contas.inserirConta(new Poupanca ("1222-7",100));
+		
+		renderJurosTotal(contas);
+		
+		
 	}
 	
-
+	public static void renderJurosTotal(RepositorioContasArray c) {
+		String numeros[] = new String[6];
+		numeros[0]="1227-7";
+		numeros[1]="1228-7";
+		numeros[2]="1225-7";
+		numeros[3]="1224-7";
+		numeros[4]="1223-7";
+		numeros[5]="1222-7";
+		
+		for(int i=0;i<6;i++) {
+			Conta a=c.procurarConta(numeros[i]);
+			if(a instanceof Poupanca) {
+				((Poupanca) a).renderJuros(2);
+			}
+		}
+	}
 }
