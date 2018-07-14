3ad81d81dda69e4ba9b1196bc2308d52f1c88fc6
Junior <momedesjr@hotmail.com>
2018-07-13 22:54:10 -0300

    Correção de pacotes.

---
+++ src/contas/Poupanca.java
@@ -0,0 +1,12 @@
+package contas;
+
+public class Poupanca extends Conta{
+   public Poupanca(String numero, double saldo){
+       super(numero, saldo);
+   }
+
+   public void renderJuros(double taxa){
+       double saldoAtual = consultarSaldo();
+       creditar(saldoAtual * taxa);
+   }
+}
