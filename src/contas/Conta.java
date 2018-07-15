ff3ea816e6947b553fe946b8d34827afeb6944f7
Junior <momedesjr@hotmail.com>
2018-07-15 11:32:50 -0300

    Classes criadas: ContaAbstrata, ContaBonificada, ContaImposto. Classes Implementadas: ContaAbstrata, ContaBonificada. Alterações feitas em: Conta, ContaPoupanca e ContaBonificada.

--- src/contas/Conta.java
+++ src/contas/Conta.java
@@ -1,36 +1,21 @@
 package contas;
 
+import clientes.Cliente;
 
-public class Conta {
-	private double saldo;
-	private String numero;
-	
-	public Conta(String numero) {
-		this.numero = numero;
-		this.saldo =0.0;
-	}
-	public Conta(String numero, double saldo) {
-		this.numero = numero;
-		this.saldo = saldo;
-	}
-	
-	public void creditar(double n) {
-		this.saldo=this.saldo+n;
-	}
-	public void debitar(double n) {
-		this.saldo=this.saldo-n;
-	}
-	
-	public double consultarSaldo() {
-		return this.saldo;
-	}
-	
-	public String getNumero() {
-		return this.numero;
-	}
-	
-	public void transferir(Conta destino, double quantia) {
-		this.debitar(quantia);
-		destino.creditar(quantia);
-	}
+public class Conta extends ContaAbstrata {
+
+    public Conta(String numero, Cliente cliente) {
+        super(numero, cliente);
+    }
+
+    public Conta(String numero, double saldo, Cliente cliente) {
+        super(numero, saldo, cliente);
+    }
+
+    @Override
+    public void debitar(double quantia) {
+        this.setSaldo(consultarSaldo() - quantia);
+    }
+
+   
 }
---
+++ src/contas/ContaAbstrata.java
@@ -0,0 +1,49 @@
+package contas;
+
+import clientes.Cliente;
+
+public abstract class ContaAbstrata {
+    private String numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public ContaAbstrata(String numero, double saldo, Cliente cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    public ContaAbstrata(String numero, Cliente cliente) {
+        this.numero = numero;
+        this.cliente = cliente;
+        saldo = 0.0;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double consultarSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+
+    public void creditar(double valor) {
+        this.saldo += valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+   
+    public void transferir(ContaAbstrata destino, double quantia) {
+        this.debitar(quantia);
+        destino.creditar(quantia);
+    }
+    public abstract void debitar(double quantia);
+    
+   
+    
+}
