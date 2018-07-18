862d7d3f0fb5a0a9dd90e919f4ef44717389a690
Junior <momedesjr@hotmail.com>
2018-07-18 19:05:24 -0300

    Alteração feita em CadConta: Adição de exceção. Alterção feita em Fachada: Adição feita em fachada e alteração do metodo consultarConta por causa da exceção.

--- src/br/com/fwintbank/model/CadConta.java
+++ src/br/com/fwintbank/model/CadConta.java
@@ -1,5 +1,6 @@
 package br.com.fwintbank.model;
 
+import br.com.fwintbank.exceptions.*;
 import br.com.fwintbank.model.Cliente;
 
 /*
@@ -7,66 +8,100 @@ import br.com.fwintbank.model.Cliente;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author felix
  */
 public class CadConta {
-   private IRepContas irep;
-   ContaAbstrata a; 
 
-   
-   public CadConta (IRepContas i){
-       this.irep= i;
-   }
-   
-   //------------------------------------- C  R  U  D -----------------------------------------------
-   public void inserirConta(ContaAbstrata c){
-       if(!this.irep.existe(c.getNumero())){
-           irep.inserir(c);       
-       }else{
-           System.out.println("conta já cadastrada");
-       }
-   }
-   
-   public void removerConta(String numero){
-       if(this.irep.existe(numero)){
-           this.irep.remover(numero);
-       }else{
-           System.out.println("Conta não encontrada");
-       }
-   }
-   
-   public void atualizarConta(ContaAbstrata c){
-       if(this.irep.existe(c.getNumero())){
-           this.irep.atualizar(c);
-       }else{
-           System.out.println("conta não encontrada!");
-       }
-   }
-   public ContaAbstrata consultarConta(String numero){
-       ContaAbstrata c=null;
-       if(this.irep.existe(numero)){
-           c= this.irep.procurar(numero);
-       }else{
-           System.out.println("conta não encontrada");
-       }
-       return c;
-   }
-   
-   //----------------------------------  R  N  --------------------------------------------
-   
-   public void debitar(ContaAbstrata c, double valor){
-       c.debitar(valor);
-   }
-   public void transferir(ContaAbstrata cOrigem, ContaAbstrata cDestino, double valor){
-       cOrigem.transferir(cDestino, valor);
-   }
-   public void creditar(ContaAbstrata c, double valor){
-       c.creditar(valor);
-   }
-   
-    
+    private IRepContas irep;
+    ContaAbstrata a;
+
+    public CadConta(IRepContas i) {
+        this.irep = i;
+    }
+
+    //------------------------------------- C  R  U  D -----------------------------------------------
+    public void inserirConta(ContaAbstrata c) throws ContaExistenteException {
+        if (!this.irep.existe(c.getNumero())) {
+            irep.inserir(c);
+        } else {
+            ContaExistenteException cee = new ContaExistenteException();
+            throw cee;
+        }
+    }
+
+    public void removerConta(String numero) throws ContaNotFoundException {
+        if (this.irep.existe(numero)) {
+            this.irep.remover(numero);
+        } else {
+            ContaNotFoundException cnfe = new ContaNotFoundException();
+            throw cnfe;
+        }
+    }
+
+    public void atualizarConta(ContaAbstrata c) throws ContaNotFoundException {
+        if (this.irep.existe(c.getNumero())) {
+            this.irep.atualizar(c);
+        } else {
+            ContaNotFoundException cnfe = new ContaNotFoundException();
+            throw cnfe;
+        }
+    }
+
+    public ContaAbstrata consultarConta(String numero) throws ContaNotFoundException {
+        ContaAbstrata c = null;
+        if (this.irep.existe(numero)) {
+            c = this.irep.procurar(numero);
+        } else {
+            ContaNotFoundException cnfe = new ContaNotFoundException();
+            throw cnfe;
+        }
+        return c;
+    }
+
+    //----------------------------------  R  N  --------------------------------------------
+    public void debitar(ContaAbstrata c, double valor) throws ContaNotFoundException, SaldoInsuficienteException {
+        if (this.irep.existe(c.getNumero())) {
+            if (valor <= c.consultarSaldo()) {
+                c.debitar(valor);
+            } else {
+                SaldoInsuficienteException sie = new SaldoInsuficienteException();
+                throw sie;
+            }
+        } else {
+            ContaNotFoundException cnfe = new ContaNotFoundException();
+            throw cnfe;
+        }
+    }
+
+    public void transferir(ContaAbstrata cOrigem, ContaAbstrata cDestino, double valor) throws ContaNotFoundException, SaldoInsuficienteException {
+        if (this.irep.existe(cOrigem.getNumero()) && this.irep.existe(cDestino.getNumero())) {
+            if (valor <= cOrigem.consultarSaldo()) {
+                cOrigem.transferir(cDestino, valor);
+            } else {
+                SaldoInsuficienteException sie = new SaldoInsuficienteException();
+                throw sie;
+            }
+        } else {
+            ContaNotFoundException cnfe = new ContaNotFoundException();
+            throw cnfe;
+        }
+    }
+
+    public void creditar(ContaAbstrata c, double valor) throws ContaNotFoundException, SaldoInsuficienteException{
+        if (this.irep.existe(c.getNumero())) {
+            if (valor <= c.consultarSaldo()) {
+                c.creditar(valor);
+            }
+            else{
+                SaldoInsuficienteException sie = new SaldoInsuficienteException();
+                throw sie;
+            }
+        } else {
+            ContaNotFoundException cnfe = new ContaNotFoundException();
+            throw cnfe;
+        }
+    }
+
 }
-
--- src/br/com/fwintbank/model/Fachada.java
+++ src/br/com/fwintbank/model/Fachada.java
@@ -14,79 +14,120 @@ import br.com.fwintbank.exceptions.*;
  * @author Maria Eduarda
  */
 public class Fachada {
+
     private static Fachada instancia;
     private CadCliente cadastroCliente;
     private CadConta cadastroContas;
-    
-    private Fachada(){
+
+    private Fachada() {
         cadastroCliente = new CadCliente(new RepositorioClienteArray());
         cadastroContas = new CadConta(new RepositorioContasArray());
     }
-    
-    public static Fachada getInstance(){
-        if(instancia == null){
+
+    public static Fachada getInstance() {
+        if (instancia == null) {
             instancia = new Fachada();
         }
         return instancia;
     }
-    
-    
+
     //OPERAÇÕES DE CONTA
-    public void inserirConta(ContaAbstrata conta){
-        cadastroContas.inserirConta(conta);
-    }
-    public void removerConta(String numero){
-        cadastroContas.removerConta(numero);
-    }
-    public ContaAbstrata consultarConta(String numero){
-        return cadastroContas.consultarConta(numero);
-    }
-    public void atualizarConta(ContaAbstrata conta){
-        cadastroContas.atualizarConta(conta);
-   }
-    public void debitar(ContaAbstrata c, double valor){
-       cadastroContas.debitar(c, valor);
-   }
-   public void transferir(ContaAbstrata cOrigem, ContaAbstrata cDestino, double valor){
-       cadastroContas.transferir(cOrigem, cDestino, valor);
-   }
-   public void creditar(ContaAbstrata c, double valor){
-       cadastroContas.creditar(c, valor);
-   }
-    
-    //OPERAÇÕES DE CLIENTE
-    
-    
-    
-     public void inserirCliente(Cliente cliente){
-        try{
-            cadastroCliente.inserir(cliente);
-        }catch(ClienteExistenteException cee){
+    public void inserirConta(ContaAbstrata conta) {
+        try {
+            cadastroContas.inserirConta(conta);
+        } catch (ContaExistenteException cee) {
             System.out.println(cee.getMessage());
         }
     }
-    public void removerliente(String cpf){
-        try{
-           cadastroCliente.remover(cpf);
-        }catch(ClienteNotFoundException cnfe){
+
+    public void removerConta(String numero) {
+        try {
+            cadastroContas.removerConta(numero);
+        } catch (ContaNotFoundException cnfe) {
             System.out.println(cnfe.getMessage());
         }
     }
-    public Cliente consultarCliente(String cpf){
-        try{
+
+    public ContaAbstrata consultarConta(String numero) {
+        ContaAbstrata conta = null;
+        try {
+            conta = cadastroContas.consultarConta(numero);
+        } catch (ContaNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        }
+        return conta;
+    }
+
+    public void atualizarConta(ContaAbstrata conta) {
+        try {
+            cadastroContas.atualizarConta(conta);
+        } catch (ContaNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        }
+    }
+
+    public void debitar(ContaAbstrata c, double valor) {
+        try {
+            cadastroContas.debitar(c, valor);
+        } catch (ContaNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        } catch (SaldoInsuficienteException sie) {
+            System.out.println(sie.getMessage());
+        }
+    }
+
+    public void transferir(ContaAbstrata cOrigem, ContaAbstrata cDestino, double valor) {
+        try {
+            cadastroContas.transferir(cOrigem, cDestino, valor);
+        } catch (ContaNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        } catch (SaldoInsuficienteException sie) {
+            System.out.println(sie.getMessage());
+        }
+    }
+
+    public void creditar(ContaAbstrata c, double valor) {
+        try {
+            cadastroContas.creditar(c, valor);
+        } catch (ContaNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        } catch (SaldoInsuficienteException sie) {
+            System.out.println(sie.getMessage());
+        }
+    }
+
+    //OPERAÇÕES DE CLIENTE
+    public void inserirCliente(Cliente cliente) {
+        try {
+            cadastroCliente.inserir(cliente);
+        } catch (ClienteExistenteException cee) {
+            System.out.println(cee.getMessage());
+        }
+    }
+
+    public void removerliente(String cpf) {
+        try {
+            cadastroCliente.remover(cpf);
+        } catch (ClienteNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        }
+    }
+
+    public Cliente consultarCliente(String cpf) {
+        try {
             return cadastroCliente.consultar(cpf);
-        }catch(ClienteNotFoundException cnfe){
+        } catch (ClienteNotFoundException cnfe) {
             System.out.println(cnfe.getMessage());
             return null; // revisar aqui depois
         }
     }
-    
-    public void atualizarCliente(Cliente cliente){
-        try{
+
+    public void atualizarCliente(Cliente cliente) {
+        try {
             cadastroCliente.atualizar(cliente);
-        }catch(ClienteNotFoundException cnfe){
+        } catch (ClienteNotFoundException cnfe) {
             System.out.println(cnfe.getMessage());
         }
     }
-    
+
 }
