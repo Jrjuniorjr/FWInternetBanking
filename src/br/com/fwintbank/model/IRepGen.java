3718472a7e1d8a7099d949209cff953ae563fe77
Jrjuniorjr <mjuniorjrjr@gmail.com>
2018-07-19 11:25:26 -0300

    Interface generica criada e implementada.

---
+++ src/br/com/fwintbank/model/IRepGen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fwintbank.model;
+
+/**
+ *
+ * @author Maria Eduarda
+ */
+public interface IRepGen<E> {
+    
+    
+    public abstract void inserir(E e);
+
+    public abstract void atualizar(E e);
+
+    public abstract void remover(String key);
+
+    public abstract boolean existe(String key);
+
+    public abstract ContaAbstrata procurar(String key);
+
+}
