111d2ebae17a64d9950a5e88ff263afb6a89731f
LLealL <llealdegoes@gmail.com>
2018-07-23 12:34:22 -0300

    implementação de métodos para conversão do enum para inteiros

--- src/br/com/fwintbank/model/TipoCliente.java
+++ src/br/com/fwintbank/model/TipoCliente.java
@@ -12,5 +12,31 @@ package br.com.fwintbank.model;
 public enum TipoCliente {
     		VIP,
 		CLASS,
-		STANDARD,
+		STANDARD;
+                
+    public int getIntTipo(TipoCliente tipo){
+        if(tipo== VIP){
+            return 1;
+        }else if(tipo==CLASS){
+            return 2;
+        }else if(tipo==STANDARD){
+            return 3;
+        }else{
+            return -1;
+        }
+    }
+    
+    public TipoCliente getTipo(int i){
+        if(i==1){
+            return VIP;
+        }else if(i==2){
+            return CLASS;
+        }else if(i==3){
+            return STANDARD;
+        }else{
+            return null;
+        }
+        
+    }
+                
 }
