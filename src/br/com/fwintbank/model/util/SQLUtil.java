902dff671ddb5c3f29a625e1d4e42360dc2d89e0
LLealL <llealdegoes@gmail.com>
2018-08-01 02:46:10 -0300

    implementação das configuraçõs de properties no pacote Util

---
+++ src/br/com/fwintbank/model/util/SQLUtil.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fwintbank.model.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author keyalisth
+ */
+public class SQLUtil {
+    private static Properties p;
+    
+    static{
+        p= new Properties();
+        try {
+            p.load(new FileInputStream("PropertiesSql.properties"));
+        } catch (FileNotFoundException ex) {
+           System.out.println("Arquivo PropertiesSql.properties nao encontrado!");
+        } catch (IOException ex) {
+           System.out.println("Erro ao ler arquivo PropertiesSql.properties");
+        }
+    }
+    
+    public static Properties getProperties(){ 
+        if(p==null){
+            p=new Properties();
+            try {
+                p.load(new FileInputStream("PropertiesSql.properties"));
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(SQLUtil.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(SQLUtil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return p;
+    }
+    
+}
