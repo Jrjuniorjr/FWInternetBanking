46d4d008d8d974d5ac7f5a60362e122c00412844
LLealL <llealdegoes@gmail.com>
2018-08-01 02:42:55 -0300

    testes da inserção de dados de cliente no main

--- src/br/com/fwintbank/Teste.java
+++ src/br/com/fwintbank/Teste.java
@@ -1,22 +1,36 @@
 
 package br.com.fwintbank;
 
-import br.com.fwintbank.dados.RepositorioClienteArray;
-import br.com.fwintbank.dados.RepositorioContasArray;
-import br.com.fwintbank.model.CadCliente;
-import br.com.fwintbank.model.CadConta;
-import br.com.fwintbank.model.Conta;
-import br.com.fwintbank.model.ContaAbstrata;
+import br.com.fwintbank.model.Cliente;
+import br.com.fwintbank.model.EnderecoCliente;
+import br.com.fwintbank.model.Fachada;
+import br.com.fwintbank.model.TipoCliente;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 public class Teste {
-        private CadCliente  cadCli;
-        private CadConta cadCont;
-        private ContaAbstrata c;
+
 
 	public static void main(String[] args) {
-        CadConta cadCont = new CadConta(new RepositorioContasArray());
-        CadCliente cadCli = new CadCliente(new RepositorioClienteArray());
-        System.out.println(""+(cadCli.consultar("22269469482")).getNome());
+            Fachada f = Fachada.getInstance();
+            EnderecoCliente e = new EnderecoCliente("1000","12","apt 2","12121");
+            Cliente c =new Cliente("12121","lucas",TipoCliente.VIP,e);
+            try {
+                f.inserirCliente(c);
+                c.setTipo(TipoCliente.CLASS);
+                f.atualizarCliente(c);
+                f.removercliente(c);
+                c.setCpf("1021");
+                f.inserirCliente(c);
+                Cliente cliente = f.consultarCliente("1021");
+                System.out.println(cliente);
+            } catch (Exception ex) {
+                Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+
         
 	}	
 }
